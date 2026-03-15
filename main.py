ff605232d1802f38d79df6e40d8b79e0698f8b44
Tipto-Ghosh <tiptoghosh@gmail.com>
2026-03-15 22:51:05 +0600

    Text Splitters added

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import os
 from dotenv import load_dotenv
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+# from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 load_dotenv()
 
-llm = HuggingFaceEndpoint(
-    repo_id="meta-llama/Llama-3.1-8B-Instruct",
-    task="text-generation",
-    max_new_tokens=512,
-    temperature = 0.7
-)
-
-model = ChatHuggingFace(llm = llm)
-result = model.invoke("Who is the CEO of OpenAI?")
-print(result.content)
+HF_API_KEY = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+# print(HF_API_KEY)
+from huggingface_hub import login
+login(HF_API_KEY)
